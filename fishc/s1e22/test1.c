5302cc2b1dcccaaa5735760d7ac8b3a00491aa1b
smileww <2293836206@qq.com>
2020-03-28 19:56:13 +0800

    Bug

--- fishc/s1e22/test1.c
+++ fishc/s1e22/test1.c
@@ -2,7 +2,16 @@
 
 int main()
 {
+	int a;
+	int *p = &a;
 
+	printf("请输入一个整数:");
+	scanf("%d", &a);
+	printf("a = %d\n", a);
+
+	printf("请重新输入一个整数;");
+	scanf("％d", p);
+	printf("a = %d\n", a);
 
 	return 0;
 }
