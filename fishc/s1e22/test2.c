b48e63a5690a6e4187346fbb88cf85aec65ce431
smileww <2293836206@qq.com>
2020-04-06 19:51:15 +0800

    新文件：   s1e22/test2.c 	新文件：   "\350\257\276\345\220\216\344\275\234\344\270\232/s1e6/a.out" 	新文件：   "\350\257\276\345\220\216\344\275\234\344\270\232/s1e6/test1.c" 	新文件：   "\350\257\276\345\220\216\344\275\234\344\270\232/s1e6/test2.c"

---
+++ fishc/s1e22/test2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	char str[128];
+
+	printf("请输入鱼Ｃ的域名：");
+	scanf("%s", str);
+
+	printf("鱼C工作室的域名是: %s\n", str);
+
+	return 0;
+}
---
+++ fishc/课后作业/s1e6/test2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	int sum, temp, weight, i;
+
+	for (i = 0; i < 64; i++)
+	{
+		
+		sum = sum + temp;
+	}
+
+	weight = sum / 25000;
+
+	printf("舍罕王应该给予达一尔%s粒麦子！\n", sum);
+	printf("如果每25000粒麦子为1kg, 那么应该给%s公斤麦子！\n", weight);
+
+	return 0;
+}
