ff664ab4fa41c90c8cad2d707139d9919ce4afe8
smileww <2293836206@qq.com>
2020-05-10 20:10:58 +0800

    修改：     a.out 	修改：     test1.c

--- fishc/s1e21/test1.c
+++ fishc/s1e21/test1.c
@@ -11,7 +11,18 @@ int main()
 	printf("a = %c\n", *pa);
 	printf("f = %d\n", *pb);
 
+	*pa = 'C';
+	*pb += 1;
 
+	printf("now, a = %c\n", *pa);
+	printf("now, f = %d\n", *pb);
+
+	printf("sizeof pa = %d\n", sizeof(pa));
+	printf("sizeof pb = %d\n", sizeof(pb));
+ 	
+	printf("the addr of is: %p\n", pa);
+	printf("the addr of is: %p\n", pb);
 
 	return 0;
 }
+
